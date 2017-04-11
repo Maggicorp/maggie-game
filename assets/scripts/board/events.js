f166d9fffb2d496540e97477bc8b3f7145d2709b
Maggicorp <mvjcasey@gmail.com>
2017-04-11 11:43:52 -0400

    Scores if you won
    Doesn't identify ties or stop game.

--- assets/scripts/board/events.js
+++ assets/scripts/board/events.js
@@ -8,9 +8,46 @@ const isFree = (idNum, arr) => {
   }
 }
 
+const stopGame = function () {
+  console.log('game is over')
+}
+
 const whoWon = function (arr) {
-  if (arr[0] === arr[1] && arr[0] === arr[2]) {
+  if (arr[0] === arr[1] && arr[0] === arr[2] && arr[0] !== '') {
     console.log(arr[0], 'wins!')
+    stopGame()
+    return
+  } else if (arr[3] === arr[4] && arr[3] === arr[5] && arr[3] !== '') {
+    console.log(arr[3], 'wins!')
+    stopGame()
+    return
+  } else if (arr[6] === arr[7] && arr[6] === arr[8] && arr[8] !== '') {
+    console.log(arr[6], 'wins!')
+    stopGame()
+    return
+  } if (arr[0] === arr[3] && arr[0] === arr[6] && arr[6] !== '') {
+    console.log(arr[0], 'wins!')
+    stopGame()
+    return
+  } else if (arr[1] === arr[4] && arr[1] === arr[7] && arr[7] !== '') {
+    console.log(arr[1], 'wins!')
+    stopGame()
+    return
+  } else if (arr[2] === arr[5] && arr[2] === arr[8] && arr[8] !== '') {
+    console.log(arr[2], 'wins!')
+    stopGame()
+    return
+  } else if (arr[0] === arr[4] && arr[0] === arr[8] && arr[8] !== '') {
+    console.log(arr[0], 'wins!')
+    stopGame()
+    return
+  } else if (arr[2] === arr[4] && arr[2] === arr[6] && arr[6] !== '') {
+    console.log(arr[2], 'wins!')
+    stopGame()
+    return
+  } else {
+    console.log('Play on')
+    return
   }
 }
 
@@ -26,7 +63,7 @@ const playO = function (ind, arr, obj) {
   arr[ind] = 'o'
   console.log(arr)
   $(obj).text('O')
- }
+}
 
 const gameArray = ['', '', '', '', '', '', '', '', '']
 
@@ -53,6 +90,7 @@ const onBoardClick = function (event) {
     }
   }
   whoWon(gameArray)
+  console.log('reach here?!?')
 }
 
 const addBoardHandlers = () => {
